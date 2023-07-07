c348e6a363602e2d830a84c5a2a84f44dce20a6a
YhyaGithub <yhyamobile57@gmail.com>
2023-07-07 17:20:52 +0300

    feat: make room picker

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+  fmt.Println("SCH - For Terminals")
+  fmt.Println()
+
+  code := roomPicker()
+  fmt.Print(code)
+}
+
+func roomPicker() string {
+  var code string
+
+  fmt.Print("Pick A Room (Room Code) : ")
+
+  fmt.Scanln(&code)
+
+  return code
+}
